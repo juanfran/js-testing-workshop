a627c6066d05a45a250e34eddcff3ec8e5a53d4a
Juanfran <juanfran.ag@gmail.com>
2014-07-03 18:48:54 +0200

    calculator tests

---
+++ test/calculatorSpec.js
@@ -0,0 +1,43 @@
+describe('test calculator', function () {
+  it('add value', function () {
+    var calculator = new Calculator();
+
+    expect(calculator.add(2)).to.equal(2);
+    expect(calculator.add(3)).to.equal(5);
+    expect(calculator.add(3, 1, 2)).to.equal(11);
+  });
+
+
+  it('sub value', function () {
+    var calculator = new Calculator(15);
+
+    expect(calculator.sub(2)).to.equal(13);
+    expect(calculator.sub(3)).to.equal(10);
+    expect(calculator.sub(3, 1, 2)).to.equal(4);
+  });
+
+  it('get history', function () {
+    var calculator = new Calculator(15);
+
+    calculator.sub(2);
+    calculator.sub(3);
+    calculator.add(3, 1);
+
+    expect(calculator.record).to.be.an('array');
+    expect(calculator.record).to.have.length(4);
+  });
+
+  it('bigger', function () {
+    var calculator = new Calculator(15);
+
+    expect(calculator.bigger(40)).to.be.true;
+    expect(calculator.bigger(10)).to.be.false;
+  });
+
+  it('smaller', function () {
+    var calculator = new Calculator(15);
+
+    expect(calculator.smaller(10)).to.be.true;
+    expect(calculator.smaller(40)).to.be.false;
+  });
+});
